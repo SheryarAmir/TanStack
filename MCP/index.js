0662d0ed841073b62dbfcc2ef1cb042c76f47df4
SheryarAmir <dev.sheryaramir@gmail.com>
2025-06-23 23:44:28 +0500

    learning MCP sERVER

---
+++ MCP/index.js
@@ -0,0 +1,51 @@
+
+
+
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+import  {z } from "zod"
+
+
+
+const server =new McpServer({
+    name:"Weather Data Featcher",
+    version : "1.0.0"
+})
+
+
+async function getWeatherByCity(city=""){
+if(city.toLocaleLowerCase()=="gilgit"){
+    return{ tem :"30c" , forcast:"change to heave raining"}
+}
+
+if(city.toLocaleLowerCase()=="hunza"){
+    return{ tem :"20c" , forcast:"change to heave winds"}
+}
+if(city.toLocaleLowerCase()=="nalter"){
+    return{ tem :"10c" , forcast:"change to heave cloudy"}
+}
+
+return{tem:null , error: "unable to get data"}
+}
+
+server.tool("getWeatherDataByCityName",{
+    city : z.string(),
+
+
+}, async({city})=>{
+
+    return{ content:[{type: "text",  text: JSON.stringify( await  getWeatherByCity(city)) }]}
+} ) 
+
+
+
+
+async function init (){
+
+    // Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
+}
+
+init()
