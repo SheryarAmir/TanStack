8ed6634051c78633f3f791c9fbda874f1b6c7bd7
SheryarAmir <dev.sheryaramir@gmail.com>
2025-09-17 20:21:01 +0500

    working with SDK

--- MCP/index.js
+++ MCP/index.js
@@ -1,51 +1,45 @@
-
-
-
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
-import  {z } from "zod"
+import { z } from "zod";
 
+const server = new McpServer({
+  name: "Weather Data Featcher",
+  version: "1.0.0",
+});
 
+async function getWeatherByCity(city = "") {
+  if (city.toLocaleLowerCase() == "gilgit") {
+    return { tem: "30c", forcast: "change to heave raining" };
+  }
 
-const server =new McpServer({
-    name:"Weather Data Featcher",
-    version : "1.0.0"
-})
+  if (city.toLocaleLowerCase() == "hunza") {
+    return { tem: "20c", forcast: "change to heave winds" };
+  }
+  if (city.toLocaleLowerCase() == "nalter") {
+    return { tem: "10c", forcast: "change to heave cloudy" };
+  }
 
-
-async function getWeatherByCity(city=""){
-if(city.toLocaleLowerCase()=="gilgit"){
-    return{ tem :"30c" , forcast:"change to heave raining"}
+  return { tem: null, error: "unable to get data" };
 }
 
-if(city.toLocaleLowerCase()=="hunza"){
-    return{ tem :"20c" , forcast:"change to heave winds"}
-}
-if(city.toLocaleLowerCase()=="nalter"){
-    return{ tem :"10c" , forcast:"change to heave cloudy"}
-}
+server.tool(
+  "getWeatherDataByCityName",
+  {
+    city: z.string(),
+  },
+  async ({ city }) => {
+    return {
+      content: [
+        { type: "text", text: JSON.stringify(await getWeatherByCity(city)) },
+      ],
+    };
+  }
+);
 
-return{tem:null , error: "unable to get data"}
-}
-
-server.tool("getWeatherDataByCityName",{
-    city : z.string(),
-
-
-}, async({city})=>{
-
-    return{ content:[{type: "text",  text: JSON.stringify( await  getWeatherByCity(city)) }]}
-} ) 
-
-
-
-
-async function init (){
-
-    // Start receiving messages on stdin and sending messages on stdout
-const transport = new StdioServerTransport();
-await server.connect(transport);
+async function init() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
 }
 
-init()
+init();
